2d53d67b4d26028e3b0b681e8118008f5a8860f9
praveengoud <praveeng5031@gmail.com>
2019-04-24 12:32:52 +0200

    first Commit

---
+++ LoginTest.java
@@ -0,0 +1,37 @@
+package homePage;
+
+import gmail.LoginPage;
+import helper.LoggerHelper;
+
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import testBase.DataSource;
+import testBase.TestBase;
+
+public class LoginTest extends TestBase {
+private final Logger log= LoggerHelper.getLogger(LoginTest.class);
+	
+	@Test
+	
+	public void testLoginApplication(){
+		
+		DataSource config=new DataSource();
+		System.out.println("driver=" + driver);
+		driver.get(config.getWebsite());
+		System.out.println("driver=" + driver); 
+		LoginPage loginpage=new LoginPage(driver);
+		
+		loginpage.enterEmailAddress("emailAddress");
+		boolean status=loginpage.verifySuccessLoginMsg();
+		
+		if(status){
+			log.info("login is successful");
+		}
+		else{
+		      Assert.assertTrue(false, "login is not successful");  
+		}
+	}
+
+}
